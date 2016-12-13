6f93b7ba90fbdb0f035f591bf5a64391295bd1ac
JuniorL <albertinolopes@hotmail.com>
2016-12-13 10:46:10 +0100

    added some javascript

---
+++ js/myScript1.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "LOVE";
+}
+
+function myFunction1() {
+   document.getElementById("demo").innerHTML = "CABO";
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
